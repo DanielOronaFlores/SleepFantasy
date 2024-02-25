31b98b66e52e614c6294313d4b02976b63bb30c1
DanielOronaFlores <a19300085@live.ceti.mx>
2024-02-25 16:18:37 -0600

    Just a little code clean up.

--- app/src/main/java/SleepTracker/Efficiency.java
+++ app/src/main/java/SleepTracker/Efficiency.java
@@ -1,7 +1,7 @@
 package SleepTracker;
 
 public class Efficiency {
-    public int getSleepEfficiency(int totalSleep, int vigil) {
+    public int getSleepEfficiency(int totalSleep, int vigil) { //TODO: Implementar este metodo al terminar el ciclo de sueño
         int PERCENTAGE = 100;
         return (totalSleep / vigil) * PERCENTAGE;
     }
