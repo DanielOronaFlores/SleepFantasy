4ffd1255355e5c16a832d6d13b73b0b4eef98764
DanielOronaFlores <a19300085@live.ceti.mx>
2024-03-06 22:48:40 -0600

    Some little fixes.

--- app/src/main/java/com/example/myapplication/Recordings.java
+++ app/src/main/java/com/example/myapplication/Recordings.java
@@ -9,7 +9,9 @@ import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Space;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,6 +33,9 @@ import Recordings.Recorders.Recorder;
 public class Recordings extends AppCompatActivity {
     private final AudiosFiles audiosFiles = new AudiosFiles();
     private final SecondsCounter secondsCounter = new SecondsCounter();
+    private Space space;
+    private AdapterLoudSounds adapterLoudSounds;
+    private RecyclerView recyclerView;
 
     PCMRecorder pcmRecorder = new PCMRecorder();
     Recorder recorder = new Recorder();
@@ -40,15 +45,21 @@ public class Recordings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recordings);
 
-        testFiltros();
+        //testFiltros();
 
-        AdapterLoudSounds adapterLoudSounds = new AdapterLoudSounds(getSoundsList());
-
-        RecyclerView recyclerView = findViewById(R.id.loudSounds_list);
+        adapterLoudSounds = new AdapterLoudSounds(getSoundsList());
+        recyclerView = findViewById(R.id.loudSounds_list);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        recyclerView.setAdapter(adapterLoudSounds);
+
+        space = findViewById(R.id.space);
+        if (getSoundsList().isEmpty()) {
+            recyclerView.setVisibility(View.GONE);
+        } else {
+            space.setVisibility(View.GONE);
+        }
 
         MediaPlayer mediaPlayer = adapterLoudSounds.getMediaPlayer();
+
 
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(this, "Se necesita otorgar el permiso de grabar audios para esta sección :)", Toast.LENGTH_SHORT).show();
@@ -72,10 +83,22 @@ public class Recordings extends AppCompatActivity {
         elimnarButton.setOnClickListener(v -> deleteRecording());
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        recyclerView.setAdapter(adapterLoudSounds);
+        //testFiltros();
+    }
+
     private void deleteRecording() {
         File file = new File(audiosFiles.getRecordingsPath());
         if (file.exists()) {
-            if (file.delete()) Toast.makeText(this, "Audio Eliminado", Toast.LENGTH_SHORT).show();
+            if (file.delete()) {
+                file = new File(audiosFiles.getXMLPath());
+                file.delete();
+                Toast.makeText(this, "Audio Eliminado", Toast.LENGTH_SHORT).show();
+                recreate();
+            }
             else Toast.makeText(this, "No se pudo eliminar el archivo", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(this, "No hay audio disponible", Toast.LENGTH_SHORT).show();
@@ -99,12 +122,5 @@ public class Recordings extends AppCompatActivity {
     private void testFiltros() {
         AudioFilter audioFilter = new AudioFilter();
         audioFilter.filterAudio();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        pcmRecorder.stopRecording();
-        recorder.stopRecording();
     }
 }
